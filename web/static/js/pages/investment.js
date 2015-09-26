2d1fb4f9903dbf024878adcfeadd8d6ce5496a77
Brobin <tobinjbrown@gmail.com>
2015-09-26 09:06:47 -0500

    built the investment future value graphs

---
+++ web/static/js/pages/investment.js
@@ -0,0 +1,61 @@
+
+var Investment = function(amount, period, rate){
+    var self = this;
+    self.amount = ko.observable(amount);
+    self.period = ko.observable(period);
+    self.rate = ko.observable(rate);
+
+    self.valueAtYear = function(year){
+        if(year > self.period()) {
+            return null;
+        }
+        var value = self.amount() * Math.pow((1+self.rate()/100), year);
+        return value.toFixed(2);
+    }
+}
+
+
+var InvestmentViewModel = function() {
+    var self = this;
+
+    self.investments = ko.observableArray([
+        new Investment(10000, 10, 9)
+    ]);
+
+    self.ykeys = ko.observableArray([0]);
+
+    self.addInvestment = function() {
+        self.ykeys.push(self.ykeys().length);
+        self.investments.push(new Investment(20000, 10, 9));
+    }
+
+    self.removeInvestment = function() {
+        if(self.investments().length > 1){
+            self.ykeys.pop();
+            self.investments.remove(this);
+        }
+    }
+
+    self.maxYears = ko.computed(function(){
+        return _.max(self.investments(), function(investment) {
+            return investment.period();
+        }).period();
+    });
+
+    self.chart = ko.computed(function() {
+        var graph = [];
+        var investments = self.investments();
+        var years = self.maxYears();
+        for (var i = 0; i < years; i++) {
+            var element = { year: i }
+            for (var j = 0; j < investments.length; j++) {
+                 element[self.ykeys()[j]] = investments[j].valueAtYear(i);
+            };
+            graph.push(element);
+        };
+        return graph;
+    });
+}
+
+
+ko.applyBindings(new InvestmentViewModel());
